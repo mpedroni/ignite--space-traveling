5f2c55be768d3e0a0dee0af40ca2d7c15a91ac85
Matheus Pedroni <mpedroni77@gmail.com>
2021-11-23 00:46:50 -0300

    Implementação do componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={commonStyles.container}>
+      <nav className={styles.header}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </nav>
+    </header>
+  );
 }
